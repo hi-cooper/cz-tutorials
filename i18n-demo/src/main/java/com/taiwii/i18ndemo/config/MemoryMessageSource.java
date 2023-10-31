710e44f48a0025911d6f2a54a6368de38a8e9aad
Cooper <hi.CooperZhu@gmail.com>
2023-10-31 16:57:22 +0800

    添加 i18n-自定义MessageSource

---
+++ i18n-demo/src/main/java/com/taiwii/i18ndemo/config/MemoryMessageSource.java
@@ -0,0 +1,28 @@
+package com.taiwii.i18ndemo.config;
+
+import org.springframework.context.support.AbstractMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@Component("messageSource")
+public class MemoryMessageSource extends AbstractMessageSource {
+    private static final Map<String, String> MESSAGES = new HashMap<>();
+    private static final Map<String, String> EN_CN = new HashMap<>();
+
+    static {
+        MESSAGES.put("en_US:language.zh_cn", "Chinese");
+        MESSAGES.put("en_US:language.en_us", "US");
+        MESSAGES.put("zh_CN:language.zh_cn", "简体中文");
+        MESSAGES.put("zh_CN:language.en_us", "英语（美国）");
+    }
+
+    @Override
+    protected MessageFormat resolveCode(String code, Locale locale) {
+        String message = MESSAGES.get(String.format("%s_%s:%s", locale.getLanguage(), locale.getCountry(), code));
+        return new MessageFormat(message, locale);
+    }
+}
